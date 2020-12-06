49fe89b9dbbc740d0d3a24fa705435ee3aab0561
iamShah <formal>
2020-12-05 19:33:09 -0500

    Done.

--- lib/reviews/api.rb
+++ lib/reviews/api.rb
@@ -1,23 +1,25 @@
-# # class API
+class API
 
-# #     def self.get_review(review)
+    def self.get_review(review)
 
-# #         key = "ernmdLZ7xu12u7kAeFqyqe9nfC4UbOhs"
-# #         url = "https://api.nytimes.com/svc/books/v3/reviews.json?author=#{review}&api-key=#{key}"
+        key = "ernmdLZ7xu12u7kAeFqyqe9nfC4UbOhs"
+        url = "https://api.nytimes.com/svc/books/v3/reviews.json?author=#{review}&api-key=#{key}"
 
-# #         response = HTTParty.get(url)
+        response = HTTParty.get(url)
 
-# #     #   response["results"].each do |e|
-# #     #     name = e["book_title"]
-# #     #     url =  e["book_author"]
-# #     #     Book.new(name, author)
+#     #   response["results"].each do |e|
+#     #     name = e["book_title"]
+#     #     url =  e["book_author"]
+#     #     Book.new(name, author)
 
-# #         binding.pry
-# #     # end
-# # end
-# # end 
-# .
+#         binding.pry
+#     # end
+# end
+# end 
+
+data["results"].each do |character_data|
+    Character.new(character_data)
+end
 
 
-key = "ernmdLZ7xu12u7kAeFqyqe9nfC4UbOhs"
-# #         url = "https://api.nytimes.com/svc/books/v3/reviews.json?author=#{review}&api-key=#{key}"
+
